f0116eb06b232677e6f223362563ab7b5b73246d
Aiden Yang <aidenhsy@gmail.com>
2020-09-13 14:12:07 +0900

    p2

--- app.js
+++ app.js
@@ -2,6 +2,17 @@
 const express = require("express");
 const ejs = require("ejs");
 const app = express();
+const fileUpload = require("express-fileupload");
+const mongoose = require("mongoose");
+
+//custom modules
+const mStorePost = require("./controllers/mStorePost");
+const mStoreUser = require("./controllers/mStoreUser");
+const mGetPost = require("./controllers/mGetPost");
+const mLoginUser = require("./controllers/mLoginUser");
+
+//custom middlewares
+const mdValidate = require("./middlewares/mdValidate");
 
 //route
 const PORT = process.env.PORT || 5000;
@@ -10,6 +21,14 @@ app.listen(PORT, console.log(`Listening on port ${PORT}`));
 //middlewares
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(fileUpload());
+app.use("/posts/store", mdValidate);
+
+//db
+mongoose.connect("mongodb://localhost/db", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 //page controllers
 const pcHome = require("./controllers/pcHome");
@@ -22,3 +41,9 @@ app.get("/", pcHome);
 app.get("/posts/create", pcCreatePost);
 app.get("/auth/login", pcLogin);
 app.get("/auth/create", pcCreateUser);
+app.get("/post/:id", mGetPost);
+
+//posts
+app.post("/posts/store", mStorePost);
+app.post("/users/store", mStoreUser);
+app.post("/users/login", mLoginUser);
---
+++ controllers/mLoginUser.js
@@ -0,0 +1,19 @@
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+
+module.exports = (req, res) => {
+  const { username, password } = req.body;
+  User.findOne({ username: username }, (error, user) => {
+    if (user) {
+      bcrypt.compare(password, user.password, (eroor, same) => {
+        if (same) {
+          res.redirect("/");
+        } else {
+          res.redirect("/auth/login");
+        }
+      });
+    } else {
+      res.redirect("/auth/login");
+    }
+  });
+};
